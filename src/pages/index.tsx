3f9436e59de7ab056d71e084521220f8a64ce454
노이(현은비) <71617021+srain66@users.noreply.github.com>
2023-09-11 12:20:47 +0900

    test: snapshot test for Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,21 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const isProduction = process.env.NODE_ENV === "production";
   return (
     <main
-      className={`w-screen h-screen flex justify-center align-center p-4 ${inter.className}`}
+      className={`w-screen h-screen flex flex-col justify-center items-center p-4 gap-2 ${inter.className}`}
     >
       This Project Only Works on Storybook
+      {isProduction ? (
+        <a href={process.env.URL}>{process.env.URL}</a>
+      ) : (
+        <pre className="w-full max-w-xs bg-black rounded p-4 text-white">
+          npm ci
+          <br />
+          npm run storybook
+        </pre>
+      )}
     </main>
   );
 }
