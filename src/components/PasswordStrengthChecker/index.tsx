1a0778be0ad3b0aa263e63ee04a4b1cecc711270
노이(현은비) <71617021+srain66@users.noreply.github.com>
2023-09-13 16:38:42 +0900

    refactor: Use common css not styled-component

--- src/components/PasswordStrengthChecker/index.tsx
+++ src/components/PasswordStrengthChecker/index.tsx
@@ -1,51 +1,27 @@
-import styled from "styled-components";
-import { useMemo } from "react";
 import { PasswordStrength } from "@/interfaces/passwordStrength";
-import { Bar, BarContainer, Header, Status, Wrapper, Text } from "./styles";
-
+import "./style.css";
+import { useMemo } from "react";
 interface IProps {
   strength: PasswordStrength;
 }
 
 export default function PasswordStrengthChecker({ strength }: IProps) {
-  const { color, width, text } = useMemo(
-    () => differentByStrength[strength],
-    [strength]
-  );
+  const strengthClass = useMemo(() => strength.toLowerCase(), [strength]);
 
   return (
-    <Wrapper>
-      <Header>
+    <div id="password-strength-checker" className="wrapper">
+      <div className="head">
         <p>Password Strength</p>
-        <Status style={{ color }}>{strength}</Status>
-      </Header>
-      <BarContainer>
-        <Bar style={{ backgroundColor: color, width }} />
-      </BarContainer>
-      <Text>{text}</Text>
-    </Wrapper>
+        <p className={`title ${strengthClass}`}>{strength}</p>
+      </div>
+      <div className="bar-container">
+        <div className={`bar ${strengthClass}`} />
+      </div>
+      <p className="description">
+        {strength === PasswordStrength.STRONG
+          ? "Your Password is STRONG!"
+          : "Your Password is easily guessable!"}
+      </p>
+    </div>
   );
 }
-
-const differentByStrength = {
-  [PasswordStrength.STRONG]: {
-    color: "#10B981",
-    width: "90%",
-    text: "Your password is STRONG!",
-  },
-  [PasswordStrength.GOOD]: {
-    color: "#3B82F6",
-    width: "60%",
-    text: "Your password is easily guessable.",
-  },
-  [PasswordStrength.FAIR]: {
-    color: "#F59E0B",
-    width: "30%",
-    text: "Your password is easily guessable.",
-  },
-  [PasswordStrength.WEAK]: {
-    color: "#EF4444",
-    width: "10%",
-    text: "Your password is easily guessable.",
-  },
-};
