ab77950b310e3bb237fdd73bf862264e3dd7d437
노이(현은비) <71617021+srain66@users.noreply.github.com>
2023-09-13 14:26:42 +0900

    fix: Config storybook to resolve alias exactly like nextjs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -11,6 +13,18 @@ const config = {
   },
   docs: {
     autodocs: "tag",
+  },
+
+  webpackFinal: async (config, { configType }) => {
+    const newConfig = { ...config };
+    newConfig.resolve.alias = {
+      ...newConfig.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+
+    console.log(newConfig);
+
+    return newConfig;
   },
 };
 export default config;
